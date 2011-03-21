b9cfcb335519e842f8ae37d12915a8dee786da11
Juho Vähä-Herttua <juhovh@iki.fi>
2011-03-21 14:54:36 +0200

    Add support for fast reduction on secp192r1 curve

--- CryptoLibrary/src/com/sun/spot/security/implementation/ecc/NIST192PrimeField.java
+++ CryptoLibrary/src/com/sun/spot/security/implementation/ecc/NIST192PrimeField.java
@@ -48,91 +48,77 @@ public final class NIST192PrimeField extends PrimeField {
      * Note that this function is FFA implementation specific and expects
      * that only the 28 LS-bits of each integer are used.
      * p = 2^192 - 2^64 - 1
-     * TODO: implement fast reduction
-     * Reduction:
-     * Input: c= (c5, c4, c3, c2, c1, c0) (base 2^64, 0 <= c < p192)
-     * 
-     *  s1 = (c2, c1, c0)
-     *  s2 = (0, c3, c3)
-     *  s3 = (c4, c4, 0)
-     *  s4 = (c5, c5, c5) 
-     * return (s1 + s2 + s3 + s4 (mod p192))
-     * 
-     *     (c2+c4+c5,c1+c3+c4+c5, c0+c3+c5) 
-     * 
      */
     protected void reduce(int[] r, int[] a) {
-        super.reduce(r,a);
         int m;
-        int h0, h1, h2, h3, h4, h5, h6, h7;
+        int h0, h1, h2, h3, h4, h5, h6;
+        int s2, s3, s4, s5, s6, s7, s8, s9;
         
-        
-        /*
         // align hi
-        h7 = ((a[13] << 8) & BMASK) | (a[12] >> 20);
-        h6 = ((a[12] << 8) & BMASK) | (a[11] >> 20);
-        //Really?
-        h5 = ((a[11] << 8) & BMASK) | (a[10] >> 20);
-        h4 = ((a[10] << 8) & BMASK) | (a[9]  >> 20);
-        h3 = ((a[9]  << 8) & BMASK) | (a[8]  >> 20);
-        h2 = ((a[8]  << 8) & BMASK) | (a[7]  >> 20);
-        h1 = ((a[7]  << 8) & BMASK) | (a[6]  >> 20);
-        h0 = ((a[6]  << 8) & BMASK) | (a[5]  >> 20);
-        // hi is in h5..h0
-        // lo is in a[5..0]
-        */
-        
-        /* 160-bit
-        // lo = lo + hi + (hi << 31);
-        m  = a[0] + h0;             r[0] = m & BMASK; m >>>= 28;
-        m += a[1] + h1 + (h0 << 3); r[1] = m & BMASK; m >>>= 28;
-        m += a[2] + h2 + (h1 << 3); r[2] = m & BMASK; m >>>= 28;
-        m += a[3] + h3 + (h2 << 3); r[3] = m & BMASK; m >>>= 28;
-        m += a[4] + h4 + (h3 << 3); r[4] = m & BMASK; m >>>= 28;
-        m += (a[5] & 0x000fffff) + h5 + (h4 << 3); r[5] = m & 0x000fffff; m >>>= 20;
-        m += (h5 << 11); h0 = m & BMASK; h1 = m >>> 28;
-        
-        // finished the first iteration. here the intermediate result
-        // is in h1:h0 and r[5..0]
+        h6 = ((a[13] << 4) & BMASK) | (a[12] >> 24);
+        h5 = ((a[12] << 4) & BMASK) | (a[11] >> 24);
+        h4 = ((a[11] << 4) & BMASK) | (a[10] >> 24);
+        h3 = ((a[10] << 4) & BMASK) | (a[9]  >> 24);
+        h2 = ((a[9]  << 4) & BMASK) | (a[8]  >> 24);
+        h1 = ((a[8]  << 4) & BMASK) | (a[7]  >> 24);
+        h0 = ((a[7]  << 4) & BMASK) | (a[6]  >> 24);
 
-        // lo = lo + hi + (hi << 31);
-        m  = r[0] + h0;             r[0] = m & BMASK; m >>>= 28;
-        m += r[1] + h1 + (h0 << 3); r[1] = m & BMASK; m >>>= 28;
-        m += r[2] +      (h1 << 3); r[2] = m & BMASK; m >>>= 28;
+        // align hi << 64
+        s9 =  (h6 >> 20);
+        s8 = ((h6 <<  8) & BMASK) | (h5 >> 20);
+        s7 = ((h5 <<  8) & BMASK) | (h4 >> 20);
+        s6 = ((h4 <<  8) & BMASK) | (h3 >> 20);
+        s5 = ((h3 <<  8) & BMASK) | (h2 >> 20);
+        s4 = ((h2 <<  8) & BMASK) | (h1 >> 20);
+        s3 = ((h1 <<  8) & BMASK) | (h0 >> 20);
+        s2 =  (h0 <<  8) & BMASK;
+
+        // hi << 64 is in s9..s2
+        // hi is in h6..h0
+        // lo is in a[6..0]
+
+        // lo = lo + hi + (hi << 64);
+        m  = a[0] + h0;      r[0] = m & BMASK; m >>>= 28;
+        m += a[1] + h1;      r[1] = m & BMASK; m >>>= 28;
+        m += a[2] + h2 + s2; r[2] = m & BMASK; m >>>= 28;
+        m += a[3] + h3 + s3; r[3] = m & BMASK; m >>>= 28;
+        m += a[4] + h4 + s4; r[4] = m & BMASK; m >>>= 28;
+        m += a[5] + h5 + s5; r[5] = m & BMASK; m >>>= 28;
+        m += (a[6] & 0x00ffffff) + h6 + s6; r[6] = m & 0x00ffffff; m >>>= 24;
+        m += ((s7 << 4) & BMASK);              h0 = m & BMASK; m >>>= 28;
+        m += ((s8 << 4) & BMASK) | (s7 >> 24); h1 = m & BMASK; m >>>= 28;
+        m += ((s9 << 4) & BMASK) | (s8 >> 24); h2 = m & BMASK;
+
+        // re-align hi << 64
+        s4 = ((h2 <<  8) + (h1 >> 20)) & BMASK;
+        s3 = ((h1 <<  8) + (h0 >> 20)) & BMASK;
+        s2 =  (h0 <<  8)               & BMASK;
+
+        // finished the first iteration. here the intermediate result
+        // is in s4:s2, h3:h0 and r[6..0]
+
+        // lo = lo + hi + (hi << 64);
+        m  = r[0] + h0;      r[0] = m & BMASK; m >>>= 28;
+        m += r[1] + h1;      r[1] = m & BMASK; m >>>= 28;
+        m += r[2] + h2 + s2; r[2] = m & BMASK; m >>>= 28;
+        m += r[3] + s3;      r[3] = m & BMASK; m >>>= 28;
+        m += r[4] + s4;      r[4] = m & BMASK; m >>>= 28;
         if (m > 0) {
-            m += r[3]; r[3] = m & BMASK; m >>= 28;
-            m += r[4]; r[4] = m & BMASK; m >>= 28;
-            r[5] += m;
+            m += r[5]; r[5] = m & BMASK; m >>>= 28;
+            r[6] += m;
         }
-        
-        */
+
         // if r >= prime then r -= prime
-        /*if ((r[5] >= 0x000fffff) 160-bit
-         && (r[4] >= 0x0fffffff)
-         && (r[3] >= 0x0fffffff)
-         && (r[2] >= 0x0fffffff)
-         && (r[1] >= 0x0ffffff7)
-         
-         && (r[0] >= 0x0fffffff)) {
-        */     
-    	if 	  ((r[6] >= 0x00FFFFFF)
-    		&& (r[5] >= 0x0FFFFFFF)
-    		&& (r[4] >= 0x0FFFFFFF)
-    		&& (r[3] >= 0x0FFFFFFF)
-    		&& (r[2] >= 0x0FFFFEFF)
-    		&& (r[1] >= 0x0FFFFFFF)
-		&& (r[0] >= 0x0FFFFFFF)) {
-    	//TODO: 
-    	    /*160
+        if (ffa.cmp(r, p) >= 0) {
             m  = r[0] + 0x00000001; r[0] = m & BMASK; m >>= 28;
-            m += r[1] + 0x00000008; r[1] = m & BMASK; m >>= 28;
+            m += r[1] + 0x00000000; r[1] = m & BMASK; m >>= 28;
+            m += r[2] + 0x00000100; r[2] = m & BMASK; m >>= 28;
             if (m > 0) {
-                m += r[2]; r[2] = m & BMASK; m >>= 28;
                 m += r[3]; r[3] = m & BMASK; m >>= 28;
                 m += r[4]; r[4] = m & BMASK; m >>= 28;
-                m += r[5]; r[5] = m & 0x000fffff;
-           }*/
+                m += r[5]; r[5] = m & BMASK; m >>= 28;
+                m += r[6]; r[6] = m & 0x00ffffff;
+            }
         }
     }
-    
 }
